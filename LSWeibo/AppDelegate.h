ef19ed32958cd16624431fc0a43440f01a0a0cf1
git <longshuijian@gmail.com>
2016-06-16 14:14:05 +0800

    Initial Commit

---
+++ LSWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LSWeibo
+//
+//  Created by 龙水健 on 16/6/16.
+//  Copyright © 2016年 龙水健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LSWeibo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LSWeibo
+//
+//  Created by 龙水健 on 16/6/16.
+//  Copyright © 2016年 龙水健. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
