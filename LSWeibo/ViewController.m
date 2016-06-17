92974db0c41bd167e024a621aaf4463fbf5493a5
longshui <longshuijian@gmail.com>
2016-06-18 07:40:23 +0800

    Initial commit

--- LSWeibo/ViewController.m
+++ LSWeibo/ViewController.m
@@ -17,12 +17,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.view.backgroundColor = [UIColor redColor];
-    NSLog(@"longsj");
+    self.view.backgroundColor = [UIColor blueColor];
     
-    NSLog(@"hongyuhang;;;");
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(ssoAuthorizeRequest)];
     
-    NSLog(@"longsj");
+}
+
+- (void)ssoAuthorizeRequest {
+    WBAuthorizeRequest *request = [WBAuthorizeRequest request];
+    request.redirectURI = kAppRedirectURI;
+    request.shouldShowWebViewForAuthIfCannotSSO = YES;
+    request.scope = @"all";
+    
+    //userInfo:用户内容，可为空，微博回调response中的requestUserInfo内容会返回来
+    request.userInfo = @{@"SSO_From": @"ViewController",
+                         @"Other_Info_1": [NSNumber numberWithInt:123],
+                         @"Other_Info_2": @[@"obj1", @"obj2"],
+                         @"Other_Info_3": @{@"key1": @"obj1", @"key2": @"obj2"}};
+    [WeiboSDK sendRequest:request];
 }
 
 - (void)didReceiveMemoryWarning {
